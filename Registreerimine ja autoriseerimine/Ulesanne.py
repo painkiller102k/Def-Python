a1deedd15c635a399cf56f2cac81fda3c10fe27a
Martin Rossakov <103848033+painkiller102k@users.noreply.github.com>
2025-04-17 13:11:28 +0300

    Update Ulesanne.py

--- Registreerimine ja autoriseerimine/Ulesanne.py
+++ Registreerimine ja autoriseerimine/Ulesanne.py
@@ -1,23 +1,29 @@
-from Moodul import registreeri_kasutaja, autoriseeri_kasutaja, muuda_parooli, unusta_parool, kontrolli_parooli, genereeri_parool, kasutajad, paroolid
+from Moodul import (
+    registreeri_kasutaja, autoriseeri_kasutaja, muuda_parooli,
+    unusta_parool, kontrolli_parooli, genereeri_parool,
+    kasutajad, paroolid, lae_andmed
+)
 
+lae_andmed()
 print("Tere tulemast!")
 
 while True:
     try:
         print("Menu:")
-        print("1. Uue kasutaja registreerimine") #регистрация нового пользователя
-        print("2. Kasutaja autoriseerimine") #авторизация пользователя
-        print("3. Parooli muutmine") #изменение пароля
-        print("4. Parooli taastamine") #восстановление пароля
-        print("5. End") #выход
-        valik = input("Teie valik: ") 
+        print("1. Uue kasutaja registreerimine")
+        print("2. Kasutaja autoriseerimine")
+        print("3. Parooli muutmine")
+        print("4. Parooli taastamine")
+        print("5. End")
+        valik = input("Teie valik: ")
 
-        if valik == "1":    
+        if valik == "1":
             print("Uue kasutaja registreerimine:")
-            username = input("Enter your usernamee: ")
+            username = input("Sisesta kasutajanimi: ")
+            email = input("Sisesta e-mail: ")
 
             if username in kasutajad:
-                print("Selle nimega kasutaja on juba olemas!") #пользователь с таким именем уже существует
+                print("Selle nimega kasutaja on juba olemas!")
             else:
                 print("Valige parooli loomise meetod:")
                 print("1. Automaatne salasõna genereerimine")
@@ -25,7 +31,7 @@ while True:
                 password_choice = input("Teie valik (1/2): ")
 
                 if password_choice == "1":
-                    password = genereeri_parool(5)
+                    password = genereeri_parool(8)
                     print(f"Teie genereeritud parool: {password}")
                 elif password_choice == "2":
                     while True:
@@ -38,14 +44,14 @@ while True:
                     print("Vale valik")
                     continue
 
-                if registreeri_kasutaja(username, password):
-                    print("Kasutaja edukalt registreeritud!")
+                if registreeri_kasutaja(username, password, email):
+                    print("Kasutaja edukalt registreeritud! Parool saadeti e-mailile.")
                 else:
                     print("Registreerimisviga!")
 
         elif valik == "2":
             print("Kasutaja autoriseerimine:")
-            username = input("Enter username ")
+            username = input("Enter username: ")
             password = input("Enter password: ")
             if autoriseeri_kasutaja(username, password):
                 print("Autoriseerimine õnnestus!")
@@ -56,12 +62,13 @@ while True:
             print("Parooli muutmine:")
             username = input("Enter username: ")
             old_password = input("Enter old password: ")
+            email = input("Enter email: ")
             if autoriseeri_kasutaja(username, old_password):
                 while True:
                     new_password = input("Sisestage uus parool: ")
                     if kontrolli_parooli(new_password):
-                        if muuda_parooli(username, new_password):
-                            print("Parool edukalt muudetud!")
+                        if muuda_parooli(username, new_password, email):
+                            print("Parool edukalt muudetud! Uus parool saadeti e-mailile.")
                             break
                         else:
                             print("Parooli muutmise viga!")
@@ -73,16 +80,16 @@ while True:
         elif valik == "4":
             print("Parool taastamine:")
             username = input("Enter username: ")
-            new_password = unusta_parool(username)
+            email = input("Enter email: ")
+            new_password = unusta_parool(username, email)
             if new_password:
-                print(f"Teie uus parool: {new_password}")
+                print("Uus parool saadeti teie e-mailile!")
             else:
                 print("Kasutajat ei leitud!")
-    
+
         elif valik == "5":
             print("Programm lõpetatud.")
             break
-
         else:
             print("Vale valik!")
 
